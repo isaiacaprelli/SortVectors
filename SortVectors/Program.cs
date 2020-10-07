8834f1232715a7c5e57191b950afbcf136975739
Isaia Caprelli <ISAIA.CAPRELLI@local.ittcesena.it>
2020-10-07 11:48:00 +0200

    second step ordinamento

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -6,12 +6,20 @@ namespace SortVectors
     {
         static void Main(string[] args)
         {
+            
             int[] vett = new int[10] { 4, -1, 100, -85, 4, 6, 77, -35, 2, 0 };
+            Ordina(vett);
             for(int i = 0; i < 10; i++)
             {
                 Console.WriteLine(vett[i]);
             }
             Console.ReadKey();
         }
+
+        static void Ordina(int[] v)
+        {
+            Array.Sort(v);
+        }
     }
+
 }
